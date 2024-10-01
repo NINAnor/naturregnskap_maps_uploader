c090e72190e95bc6350ffa1049d12e12d0aaccfb
Niccolò Cantù <niccolo.cantu@nina.no>
2024-10-01 16:03:22 +0200

    simplify nix shell

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -1,28 +1,14 @@
+with import <nixpkgs> { };
+
 let
-  pkgs = import <nixpkgs> {};
-  python = pkgs.python312;
-  pythonPackages = python.pkgs;
-in with pkgs; mkShell {
-  packages = [
-    python
-    pythonPackages.venvShellHook
+  pythonPackages = python3Packages;
+in pkgs.mkShell rec {
+  buildInputs = [
+    pythonPackages.python
+    pythonPackages.fiona
+    pythonPackages.click
+    pythonPackages.django-environ
     gdal
     tippecanoe
   ];
-
-  shellHook = ''
-    SOURCE_DATE_EPOCH=$(date +%s)
-    VENV=.venv
-
-    if test ! -d $VENV; then
-      python3 -m venv $VENV
-    fi
-    source ./$VENV/bin/activate
-    export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
-    pip install -e .
-  '';
-
-  postShellHook = ''
-    ln -sf ${python.sitePackages}/* ./.venv/lib/python3.12/site-packages
-  '';
 }
